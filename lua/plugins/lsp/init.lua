4c8f4ff579d878b430f8fb9f299250689c1e3bd8
moozd <mohammad.mdz72@gmail.com>
2024-05-28 23:50:48 +0200

    update

--- lua/plugins/lsp/init.lua
+++ lua/plugins/lsp/init.lua
@@ -56,7 +56,7 @@ return {
         mapping = cmp.mapping.preset.insert({
           ["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
           ["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
-          ["<C-y>"] = cmp.mapping.confirm({
+          ["<CR>"] = cmp.mapping.confirm({
             behavior = cmp.ConfirmBehavior.Insert,
             select = true,
           }),
