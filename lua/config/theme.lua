dea954ad011b695ed065e902d0cbff4eda83e4dd
mo <mohammad.mdz72@gmail.com>
2024-05-30 13:34:08 +0200

    fix thme

--- lua/config/theme.lua
+++ lua/config/theme.lua
@@ -1,11 +1,11 @@
 vim.g.zenbones_italc_comments = false
 vim.g.zenbones_darkness = 'stark'
-vim.cmd.colorscheme("rose-pine")
+vim.cmd.colorscheme("catppuccin")
 
 -- remove background
-vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+-- vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 -- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
+-- vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
 
 vim.cmd("highlight DiagnosticUnderlineInfo cterm=undercurl gui=undercurl")
 vim.cmd("highlight DiagnosticUnderlineError cterm=undercurl gui=undercurl")
