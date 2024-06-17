d38d570376a383640370f611ef12ac50642bdddc
mo <mohammad.mdz72@gmail.com>
2024-06-18 01:43:08 +0200

    mini.ai

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -108,6 +108,10 @@ return {
   },
   {
     "echasnovski/mini.ai",
+    version = false,
+    config=function ()
+        require("mini.ai").setup()
+    end
   },
   {
     "smjonas/inc-rename.nvim",
