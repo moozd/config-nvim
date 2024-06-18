46305dcf67ee47c99c6ceb75937931f70ce31c70
mo <mo@mos-MacBook-Pro.local>
2024-06-18 17:35:48 +0200

    :rocket:

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,13 +1,13 @@
 return {
 
-  "rose-pine/nvim",
-  name = "rose-pine",
-  config = function()
-    require("rose-pine").setup({
-      styles = {
-        italic = false,
-        transparency = true,
-      },
-    })
-  end,
+  "catppuccin/nvim",
+  lazy = false,
+  -- config = function()
+  --   -- require("rose-pine").setup({
+  --   --   styles = {
+  --   --     italic = false,
+  --   --     transparency = true,
+  --   --   },
+  --   -- })
+  -- end,
 }
